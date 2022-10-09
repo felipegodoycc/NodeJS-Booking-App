3247dc92813de47a18c2c33b093c127557a3e4b1
Felipe Godoy <felipe.godoy@ceinf.cl>
2022-10-09 19:13:39 -0300

    fix para permitir aceptar data proveniente del body

--- ReqHandlers/POST-Handlers/book.js
+++ ReqHandlers/POST-Handlers/book.js
@@ -14,9 +14,10 @@ const TIMESLOTS_PATH = './Utility/timeslots.json';
  * @param {number} minute  Minute of the timeslot to search for.
  * @returns {object}  The timeslot object that was found. If nothing was found, returns undefined.
  */
-function findMatchingTimeslot(timeslots, year, month, day, hour, minute) {
+function findMatchingTimeslot(timeslots, { year, month, day, hour, minute }) {
+    console.log("[findMatchingTimeslot] Datos: ", year, month, day, hour, minute);
     const timeslotDate = new Date(Date.UTC(year, month-1, day, hour, minute)).toISOString();
-    console.log("Timeslot a reservar: ", timeslotDate)
+    console.log("[findMatchingTimeslot] Timeslot a reservar: ", timeslotDate)
     const foundTimeslot = timeslots.find(function (element) {
         //const elementDate = new Date(element.startTime).toISOString(); // Ensure matching ISO format.
         return element.startTime.includes(hour + ':' + minute  + ':00');
@@ -28,32 +29,37 @@ function findMatchingTimeslot(timeslots, year, month, day, hour, minute) {
 /**
  * Books an appointment using the given date and time information.
  * @param {object} auth  The oAuth2Client used for authentication for the Google Calendar API.
- * @param {number} year  Year of the timeslot to book.
- * @param {number} month  Month of the timeslot to book.
- * @param {number} day  Day of the timeslot to book.
- * @param {number} hour  Hour of the timeslot to book.
- * @param {number} minute  Minute of the timeslot to book.
+ * @param {object} eventData
+ * @param {object} personalData
  * @returns {promise}  A promise representing the eventual completion of the bookAppointment() function.
  */
-function bookAppointment(auth, year, month, day, hour, minute) {
+function bookAppointment(auth, eventData, personalData) {
+    // TO DO: Validar si el slot esta disponible
     return new Promise(function(resolve, reject) {
-        const isInvalid = reqValidator.validateBooking(year, month, day, hour, minute);
+        console.log("[bookAppointment] Event data: ", eventData);
+        console.log("[bookAppointment] User data: ", personalData);
+        const parsedEventData = appUtil.parseBookingData(eventData);
+        const isInvalid = reqValidator.validateBooking(parsedEventData);
         if (isInvalid) return reject(isInvalid);
 
         const timeslots = (JSON.parse(fs.readFileSync(TIMESLOTS_PATH))).timeslots;
-        const timeslot = findMatchingTimeslot(timeslots, year, month, day, hour, minute);
+        const timeslot = findMatchingTimeslot(timeslots, parsedEventData);
         if (!timeslot) return resolve({success: false, message: 'Invalid time slot'});
-        const date = year + '-' + month + '-' + day;
-        console.log("Timeslot encontrado");
-        const event = appUtil.makeEventResource(date, timeslot.time.startTime, timeslot.time.endTime);
-        console.log("Evento: ", event)
+        const date = eventData.date;
+        const event = appUtil.makeEventResource(date, timeslot.time.startTime, timeslot.time.endTime, personalData);
+        console.log("[bookAppointment] Evento: ", event)
         const calendar = google.calendar({version: 'v3', auth});
         calendar.events.insert({
             auth: auth,
             calendarId: 'primary',
+            sendNotifications: true,
+            sendUpdates: 'all',
             resource: event
         }, function (err, res) {
-            if (err) return console.log('Error contacting the Calendar service: ' + err);
+            if (err){
+                resolve({ success: false, message: 'Error GCalendar API'})
+                return console.log('[bookAppointment] Error contacting the Calendar service: ' + JSON.stringify(err))
+            };
             const event = res.data;
             console.log('Appointment created: ', event.id);
             const result = {startTime: event.start.dateTime, endTime: event.end.dateTime};
